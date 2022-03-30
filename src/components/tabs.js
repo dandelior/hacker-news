b54dd32e539bada7144c145d532ff18a5805f8ed
dandelior <jberrioscifuentes@gmail.com>
2022-03-30 18:52:32 -0300

    styles added

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "../styles/components/tabs.sass";
+
 const Tabs = () => {
   return (
     <div className="tabs">
-      <div className="tab">All</div>
+      <div className="tab active">All</div>
       <div className="tab">My faves</div>
     </div>
   );
