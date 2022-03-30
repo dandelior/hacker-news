8a51ca497d955989a0d7d6c7dec1bbd07c49fb97
dandelior <jberrioscifuentes@gmail.com>
2022-03-30 18:31:42 -0300

    header styles added and set

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "../styles/components/header.sass";
+
 import Logo3x from "../assets/img/hacker-news@3x.png";
 import Logo2x from "../assets/img/hacker-news@2x.png";
 import Logo from "../assets/img/hacker-news.png";
@@ -7,11 +9,13 @@ import Logo from "../assets/img/hacker-news.png";
 const Header = () => {
   return (
     <header>
-      <picture>
-        <source srcset={Logo3x} media="(min-width: 1024px)" />
-        <source srcset={Logo2x} media="(min-width: 768px)" />
-        <img src={Logo} alt="Logo" />
-      </picture>
+      <a href="/">
+        <picture>
+          <source srcset={Logo3x} media="(min-width: 1024px)" />
+          <source srcset={Logo2x} media="(min-width: 768px)" />
+          <img src={Logo} alt="Logo" />
+        </picture>
+      </a>
     </header>
   );
 };
