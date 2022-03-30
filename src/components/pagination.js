e729b9b5f5fd09b7fa7f912dc62f39a3758accf5
dandelior <jberrioscifuentes@gmail.com>
2022-03-30 19:50:08 -0300

    styles and markup added

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import "../styles/components/pagination.sass";
+
 const Pagination = () => {
-  return <div className="pagination"></div>;
+  return (
+    <div className="pagination">
+      <div className="page-item">&lt;</div>
+      <div className="page-item active">1</div>
+      <div className="page-item">2</div>
+      <div className="page-item">3</div>
+      <div className="page-item">&gt;</div>
+    </div>
+  );
 };
 
 export default Pagination;
