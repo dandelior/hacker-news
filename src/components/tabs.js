bab178f061c0b0a46da733150f812bbd76577c7b
dandelior <jberrioscifuentes@gmail.com>
2022-03-30 23:43:58 -0300

    tab logic at click

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/components/tabs.sass";
 
-const Tabs = () => {
+const Tabs = ({ detectClick }) => {
+  const [activeAllTab, setActiveAllTab] = useState(true);
+  const [activeFavsTab, setActiveFavsTab] = useState(false);
+
+  const triggerActiveAllTab = () => {
+    setActiveAllTab(true);
+    setActiveFavsTab(false);
+    detectClick("all");
+  };
+
+  const triggerActiveFavsTab = () => {
+    setActiveFavsTab(true);
+    setActiveAllTab(false);
+    detectClick("favs");
+  };
+
   return (
     <div className="tabs">
-      <div className="tab active">All</div>
-      <div className="tab">My faves</div>
+      <div
+        className={`tab ${activeAllTab && "active"}`}
+        onClick={triggerActiveAllTab}
+      >
+        All
+      </div>
+      <div
+        className={`tab ${activeFavsTab && "active"}`}
+        onClick={triggerActiveFavsTab}
+      >
+        My faves
+      </div>
     </div>
   );
 };
