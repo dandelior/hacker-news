951f6ca4bf8c788c517f1df86c41ab126bef2384
dandelior <jberrioscifuentes@gmail.com>
2022-03-31 01:57:09 -0300

    pagination and selection filter working

--- src/components/newsGrid.js
+++ src/components/newsGrid.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from "react";
 import Newsitem from "./newsItem";
+import Selector from "./selector";
+import Pagination from "./pagination";
 
 import "../styles/components/newsGrid.sass";
 
-const Newsgrid = ({ query, page }) => {
+const Newsgrid = () => {
   const [data, setData] = useState(null);
+  const [query, setQuery] = useState("angular");
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     const getData = async () => {
-      return await fetch(
+      fetch(
         `https://hn.algolia.com/api/v1/search_by_date?query=${query}&hitsPerPage=8&page=${page}`
       )
         .then((response) => response.json())
@@ -17,23 +21,37 @@ const Newsgrid = ({ query, page }) => {
     getData();
   }, [query, page]);
 
+  const atSelectFilter = (e) => {
+    setQuery(e);
+    setPage(0);
+  };
+  const atPageChange = (e) => setPage(e);
+
   console.log(data);
 
   return (
-    <div className="news-grid">
-      {data &&
-        data.hits.map((news, index) => {
-          return (
-            <Newsitem
-              title={news.story_title}
-              link={news.story_url}
-              createdAt={news.created_at}
-              author={news.author}
-              key={index}
-            />
-          );
-        })}
-    </div>
+    <>
+      <Selector filterSelected={atSelectFilter} />
+      <div className="news-grid">
+        {data &&
+          data.hits.map((news, index) => {
+            return (
+              <Newsitem
+                title={news.story_title}
+                link={news.story_url}
+                createdAt={news.created_at}
+                author={news.author}
+                key={index}
+              />
+            );
+          })}
+      </div>
+      <Pagination
+        pageSelected={atPageChange}
+        totalPages={data && data.nbPages}
+        actualPage={data && data.page}
+      />
+    </>
   );
 };
 
